46e7eb8cebd5736446ed194532e5b4de1d7550d1
Salim Ghanem <saleemghanem@gmail.com>
2021-05-07 12:57:40 +0300

    reducers, store

--- src/index.js
+++ src/index.js
@@ -2,12 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+//redux setup
 
+import { createStore } from "redux";
+import rootReducer from "./reducers";
+
+import { Provider } from "react-redux";
+
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById("root"),
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/reducers/gamesReducer.js
@@ -0,0 +1,15 @@
+const initState = {
+  popular: [],
+  newGames: [],
+
+  upcoming: [],
+  searched: [],
+};
+export const gamesReducer = (state = initState, action) => {
+  switch (action.type) {
+    case "FETCH_GAMES":
+      return { ...state };
+    default:
+      return { ...state };
+  }
+};
